a95b5725d479e59319eb4d2a85178607cefdfea8
Ryan Kroon <rykroon.tech@gmail.com>
2021-06-27 14:14:28 -0400

    Refactored throttling logic.

--- flaskrest/throttling.py
+++ flaskrest/throttling.py
@@ -1,79 +1,62 @@
+from functools import wraps
 import time
-from flask import current_app, g, request
+from flask import g
+from werkzeug.exceptions import TooManyRequests
 
 
-def get_remote_addr():
-    if request.access_route:
-        return ''.join(request.access_route)
+class Throttle:
+    def __init__(self, scopes, second=None, minute=None, hour=None, day=None):
+        self.scopes = scopes
 
-    return request.remote_addr
+        self.durations = {
+            86400: day,
+            3600: hour,
+            60: minute,
+            1: second
+        }
 
+        # Remove durations without a specified number of requests
+        self.durations = {k: v for k, v in self.durations.items() if v is not None}
 
-class BaseThrottle:
-
-    rate = None
-    scope = None
-
-    def __init__(self):
-        self.num_requests, self.duration = self.parse_rate(self.rate)
-
-    def parse_rate(self, rate):
-        num, period = rate.split('/')
-        num_requests = int(num)
-        duration = {'s': 1, 'm': 60, 'h': 3600, 'd': 86400}[period[0]]
-        return (num_requests, duration)
-    
-    def get_ident(self):
-        return None
-
-    def get_cache_key(self):
-        return 'throttle:{}:{}'.format(self.scope, self.get_ident())
-
-    def get_from_cache(self, key, default=None):
-        raise NotImplementedError
-
-    def set_to_cache(self, key, value, timeout):
-        raise NotImplementedError
+    def __call__(self, func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if not self.allow_request():
+                raise TooManyRequests
+            return func(*args, **kwargs)
+        return wrapper
 
     def allow_request(self):
-        raise NotImplementedError
+        # If the scopes do not match then the request
+        # will not be throttled. 
+        for scope in self.scopes:
+            if scope not in g.auth.scopes:
+                return True
 
-    def retry_after(self):
-        raise NotImplementedError
+        cache_key = 'throttle:{user}:{scopes}'.format(
+            user=g.user.identifier, 
+            scopes=''.join(self.scopes)
+        )
 
+        history = cache.get(cache_key, [])
+        now = time.time()
 
-class SlidingWindowThrottle(BaseThrottle):
+        # "slide" the window based on the max duration
+        max_duration = max(self.durations)
+        while history and history[-1] <= now - max_duration:
+            history.pop()
 
-    def allow_request(self):
-        if self.rate is None:
-            return True
+        for duration, num_requests in self.durations.items():
+            end_index = len(history) - 1
+            while history and history[end_index] <= now - duration:
+                end_index -= 1
 
-        #if the get_ident() function returns None
-        # then there is no identity to rate limit so return True
-        if self.get_ident() is None:
-            return True
+            if len(history[:end_index]) + 1 >= num_requests:
+                return False
 
-        self.key = self.get_cache_key()
-
-        self.history = self.get_from_cache(self.key, [])
-        self.now = time.time()
-
-        # "slide" the window
-        while self.history and self.history[-1] <= self.now - self.duration:
-            self.history.pop()
-
-        if len(self.history) >= self.num_requests:
-            return self.throttle_failure()
-
-        return self.throttle_success()
-
-    def throttle_success(self):
-        self.history.insert(0, self.now)
-        self.set_to_cache(self.key, self.history, self.duration)
+        history.insert(0, now)
+        cache.set(cache_key, history, max_duration)
         return True
-
-    def throttle_failure(self):
-        return False
 
     def retry_after(self):
         if self.history:
@@ -87,19 +70,3 @@ class SlidingWindowThrottle(BaseThrottle):
 
         return remaining_duration / float(available_requests)
 
-
-class AnonThrottle(SimpleRateThrottle):
-    scope = 'anon'
-
-    def get_ident(self):
-        if g.user:
-            return None
-
-        return get_remote_addr()
-
-
-class UserThrottle(SimpleRateThrottle):
-    scope = 'user'
-
-    def get_ident(self):
-        return g.user
