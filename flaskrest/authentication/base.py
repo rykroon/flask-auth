3df84047371ede7df8d08759d20db5f7d4a64859
Ryan Kroon <rykroon.tech@gmail.com>
2021-06-28 08:38:35 -0400

    Added Scheme, Basic, and Bearer Authentication classes.

--- flaskrest/authentication/base.py
+++ flaskrest/authentication/base.py
@@ -1,3 +1,5 @@
+from base64 import b64decode
+from flask import request
 
 
 class BaseAuthentication:
@@ -7,3 +9,47 @@ class BaseAuthentication:
 
     def authenticate(self):
         raise NotImplementedError
+
+
+class SchemeAuthentication(BaseAuthentication):
+    scheme = None
+    realm = None
+
+    @property
+    def www_authenticate(self):
+        realm = self.realm or request.host_url
+        return '{} realm={}'.format(self.scheme, realm)
+
+    def authenticate(self):
+        authorization = request.headers.get('Authorization')
+        if authorization is None:
+            return
+
+        scheme, _, credentials = authorization.partition(' ')
+        if scheme.lower() != self.scheme:
+            return
+
+        return self.validate_credentials(credentials)
+
+    def validate_credentials(self, credentials):
+        raise NotImplementedError
+
+
+class BasicAuthentication(SchemeAuthentication):
+    scheme = 'basic'
+
+    def validate_credentials(self, credentials):
+        decoded_credentials = b64decode(credentials)
+        username, _, password = decoded_credentials.partition(':')
+
+        return self.validate_user(username, password)
+
+    def validate_user(self, username, password):
+        raise NotImplementedError
+
+
+class BearerAuthentication(SchemeAuthentication):
+    scheme = 'bearer'
+
+    def validate_credentials(self, credentials):
+        pass
