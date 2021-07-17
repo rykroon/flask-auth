597b87b32b4903df6e2e2b4a4020dd45f7a7e09f
Ryan Kroon <rykroon.tech@gmail.com>
2021-07-17 13:28:33 -0400

    Added new throttling changes to main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask
 from flask.views import MethodView
 
 from flaskauth import AuthenticationMiddleware
-from flaskauth.permissions import AllowAny, IsAuthenticated
-from flaskauth.throttling import SimpleThrottle, AnonThrottle
+from flaskauth.permissions import AllowAny
+from flaskauth.throttling import SimpleThrottle, rate_limit
 
 app = Flask(__name__)
 
@@ -19,8 +19,6 @@ def init_middleware():
 cache = SimpleCache()
 
 class MySimpleThrottle(SimpleThrottle):
-    minute = 10
-    second = 1
 
     @property
     def cache(self):
@@ -29,7 +27,7 @@ class MySimpleThrottle(SimpleThrottle):
 
 @app.route('/', methods=['get'])
 @AllowAny
-@MySimpleThrottle
+@rate_limit(MySimpleThrottle, minute=10, second=1)
 def root():
     return "OK"
 
