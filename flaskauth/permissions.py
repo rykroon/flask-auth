0f008d2734d0144b4d01aaabb535ec08939d0adb
Ryan Kroon <rykroon.tech@gmail.com>
2021-07-18 09:49:58 -0400

    Added permission decorator.

--- flaskauth/permissions.py
+++ flaskauth/permissions.py
@@ -1,4 +1,4 @@
-from functools import update_wrapper
+from functools import wraps
 from flask import g, request
 from werkzeug.exceptions import Forbidden
 
@@ -6,17 +6,19 @@ from werkzeug.exceptions import Forbidden
 SAFE_METHODS = ('GET', 'HEAD', 'OPTIONS')
 
 
+def permission(permission_class, **permission_kwargs):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(args, **kwargs):
+            perm = permission_class(**permission_kwargs)
+            if not perm.has_permission():
+                raise Forbidden
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
 class BasePermission:
-
-    def __init__(self, func):
-        update_wrapper(self, func)
-        self.func = func
-    
-    def __call__(self, *args, **kwargs):
-        if not self.has_permission():
-            raise Forbidden
-        return self.func(*args, **kwargs)
-
     def has_permission(self):
         raise NotImplementedError
 
@@ -40,3 +42,8 @@ class IsAuthenticatedOrReadOnly(BasePermission):
     def has_permission(self):
         return bool(request.method in SAFE_METHODS or g.user and g.user.is_authenticated)
 
+
+allow_any = permission(AllowAny)
+is_authenticated = permission(IsAuthenticated)
+is_admin = permission(IsAdmin)
+is_authenticated_or_read_only = permission(IsAuthenticatedOrReadOnly)
