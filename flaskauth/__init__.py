844dcf8729f5ac1439c0eb2265d7247bc8ed32b0
Ryan Kroon <rykroon.tech@gmail.com>
2021-07-18 09:55:30 -0400

    Added new decorators to __init__.py

--- flaskauth/__init__.py
+++ flaskauth/__init__.py
@@ -1,3 +1,4 @@
 from flaskauth.authentication import *
-from flaskauth.permissions import BasePermission, AllowAny, IsAuthenticated, IsAdmin
+from flaskauth.permissions import BasePermission, AllowAny, IsAuthenticated, \
+    IsAdmin, allow_any, is_authenticated, is_admin, is_authenticated_or_read_only
 from flaskauth.throttling import BaseThrottle, SimpleThrottle, AnonThrottle
